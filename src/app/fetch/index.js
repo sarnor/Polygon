aa0940090b256e6c1992ba16734426564f362407
sarnor <sarnor@mail.ru>
2025-11-25 14:16:29 +0400

    25-11-2025 14:16

---
+++ src/app/fetch/index.js
@@ -0,0 +1,4 @@
+export const fetchRequest = (m, u) => {
+    return fetch(u)
+}
+
--- src/app/index.js
+++ src/app/index.js
@@ -3,3 +3,44 @@ import '../styles/less/index.less';
 import '../styles/scss/index.scss';
 import '../styles/sass/index.sass';
 import '../resource/fontawesome-pro-6.2.1-web/js/all.js';
+
+
+let maimPromise, tracksWrapper, trackList;
+
+
+const buildListRadioChannels = (items) => {
+    items.forEach((element, index) => {
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('track');
+        newDiv.innerHTML = `<span>${index + 1}</span> <span>${element.name}</span>`
+        tracksWrapper.insertAdjacentElement('beforeend', newDiv.cloneNode(true))
+    });
+}
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    import('./channels/index.js')
+        .then(data => {
+            tracksWrapper = document.querySelector('.tracks-wrapper')
+            return data
+        })
+        .then(data => {
+            buildListRadioChannels(data.channels)
+        })
+        .then(() => {
+            trackList = document.querySelectorAll('.track')
+            console.log(trackList);
+        })
+        .then(() => {
+            document.body.addEventListener('click', () => {
+                console.log('click body');
+                tracksWrapper.classList.toggle('center')
+
+            })
+        })
+        .catch(err => {
+            console.log(err, 'Error');
+        })
+})
