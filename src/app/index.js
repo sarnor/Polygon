3028aa213478370db47258f0ce5784c832d83389
sarnor <sarnor@mail.ru>
2026-02-15 13:27:39 +0400

    15-02-2026 13:27

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,9 @@
 let maimPromise, tracksWrapper, trackList, loading, pagePromise;
-import '../styles/css/index.css';
-import '../styles/less/index.less';
-import '../styles/scss/index.scss';
-import '../styles/sass/index.sass';
-import '../resource/fontawesome-pro-6.2.1-web/js/all.js';
 
 import { httpAjax } from './httpAjax/index.js'
 
+import { channels } from './channels/index.js';
 
-
-
-maimPromise = new Promise((resolve, reject) => {
-    import('./channels/index.js')
-        .then(e => resolve(e.channels))
-})
 
 const buildListRadioChannels = (items) => {
     items.forEach((element, index) => {
@@ -23,51 +13,52 @@ const buildListRadioChannels = (items) => {
         tracksWrapper.insertAdjacentElement('beforeend', newDiv.cloneNode(true))
     });
 }
-
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-
-
-    maimPromise
-        .then(data => {
-            loading = document.querySelector('.loader');
-            return data
-        })
-        .then(data => {
-            tracksWrapper = document.querySelector('.tracks-wrapper')
-            return data
-        })
-        .then(data => {
-            buildListRadioChannels(data)
-            console.log("🚀 ~ data:", data)
-        })
-        .then(() => {
-            trackList = document.querySelectorAll('.track')
-
-        })
-        .then(() => {
-            document.body.addEventListener('click', () => {
-                console.log('click body');
-                tracksWrapper.classList.toggle('center')
-            })
-        })
-        .then(() => {
-            httpAjax('GET', '/views/gallery/index.html', 'text')
-                .then(html => {
-                    console.log('HTML length:', html.length);
-                    console.log('HTML', html);
-                    // Можно вставить в DOM
-                    document.querySelector('main').insertAdjacentHTML('beforeend', html)
-                })
-                .catch(err => console.error('AJAX error:', err));
-
-        })
-        .finally(() => {
-            loading.classList.add('remove')
-        })
-        .catch(err => {
-            console.log(err, 'Errddddddddddddddddddddddddddddddddddddor');
-        })
+document.body.addEventListener('pointerdown', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+        tracksWrapper.classList.add('center')
+    }, 3000);
 })
+document.body.addEventListener('click', () => {
+    tracksWrapper.classList.remove('center')
+})
+
+
+window.addEventListener('DOMContentLoaded', () => { })
+maimPromise = new Promise((resolve, reject) => {
+    resolve(channels)
+})
+
+maimPromise
+    .then(data => {
+        loading = document.querySelector('.loader');
+        tracksWrapper = document.querySelector('.tracks-wrapper')
+        return data
+    })
+    .then(data => {
+        buildListRadioChannels(data)
+    })
+    .then(() => {
+        trackList = document.querySelectorAll('.track')
+    })
+    .finally(() => {
+        loading.classList.add('remove')
+    })
+    .catch(err => {
+        console.error(err, 'Error');
+    })
+document.addEventListener('click', e => {
+    const link = e.target.closest('a[href]');
+    if (!link) return;
+    e.preventDefault();
+
+    const href = link.getAttribute('href');
+
+    // Если есть хэш — скроллим к элементу
+    if (hasHash) {
+        setTimeout(() => {
+            const el = document.querySelector(url.hash);
+            if (el) el.scrollIntoView({ behavior: 'smooth' });
+        }, 50);
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -34,119 +34,119 @@ const webpackConfig = {
         publicPath: '/'
     },
     module: {
-            rules: [{
-                        test: /\.tsx?$/,
-                        use: 'ts-loader',
-                        exclude: /node_modules/,
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+        {
+            test: /\.less$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'postcss-loader',
+                'less-loader',
+            ],
+        },
+        {
+            test: /\.sass$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'postcss-loader',
+                'sass-loader',
+            ],
+        },
+        {
+            test: /\.scss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'postcss-loader',
+                'sass-loader',
+            ],
+        },
+        {
+            test: /\.(svg|png|jpg|jpeg|webp|gif|icon)$/i,
+            type: 'asset/resource',
+            generator: {
+                filename: path.join('[path][name][ext]'),
+            },
+        },
+        {
+            test: /\.(mp3|wav|ogg)$/,
+            include: [
+                path.join(__dirname, 'src', "media", "music"),
+            ],
+            use: [{
+                loader: "file-loader",
+                options: {
+                    name: "media/music/[name].[ext]",
+                },
+            },],
+        },
+        {
+            test: /\.(mp4|webm|ogv|3GP)$/,
+            include: [
+                path.join(__dirname, 'src', "media", "video"),
+            ],
+            use: [{
+                loader: "file-loader",
+                options: {
+                    name: "media/video/[name].[ext]",
+                },
+            },],
+        },
+        {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            include: [
+                path.resolve(__dirname, 'src', "fonts"),
+            ],
+            use: [{
+                loader: "file-loader",
+                options: {
+                    name: "resource/fonts/[name].[ext]",
+                },
+            },],
+        },
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+            options: {
+                sources: {
+                    list: [{
+                        tag: 'source',
+                        attribute: 'src-set',
+                        type: 'srcset'
                     },
                     {
-                        test: /\.css$/i,
-                        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+                        tag: 'img',
+                        attribute: 'data-src',
+                        type: 'src'
                     },
-            {
-                test: /\.less$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'less-loader',
-                ],
-            },
-            {
-                test: /\.sass$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.scss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(svg|png|jpg|jpeg|webp|gif|icon)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: path.join('[path][name][ext]'),
+                    {
+                        tag: 'img',
+                        attribute: 'src',
+                        type: 'src'
+                    },
+                    {
+                        tag: 'source',
+                        attribute: 'srcset',
+                        type: 'srcset'
+                    },
+                    {
+                        tag: 'source',
+                        attribute: 'data-srcset',
+                        type: 'srcset'
+                    }
+                    ],
                 },
             },
-            {
-                test: /\.(mp3|wav|ogg)$/,
-                include: [
-                    path.join(__dirname, 'src', "media", "music"),
-                ],
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "media/music/[name].[ext]",
-                    },
-                }, ],
-            },
-            {
-                test: /\.(mp4|webm|ogv|3GP)$/,
-                include: [
-                    path.join(__dirname, 'src', "media", "video"),
-                ],
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "media/video/[name].[ext]",
-                    },
-                }, ],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                include: [
-                    path.resolve(__dirname, 'src', "fonts"),
-                ],
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "resource/fonts/[name].[ext]",
-                    },
-                }, ],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-                options: {
-                    sources: {
-                        list: [{
-                                tag: 'source',
-                                attribute: 'src-set',
-                                type: 'srcset'
-                            },
-                            {
-                                tag: 'img',
-                                attribute: 'data-src',
-                                type: 'src'
-                            },
-                            {
-                                tag: 'img',
-                                attribute: 'src',
-                                type: 'src'
-                            },
-                            {
-                                tag: 'source',
-                                attribute: 'srcset',
-                                type: 'srcset'
-                            },
-                            {
-                                tag: 'source',
-                                attribute: 'data-srcset',
-                                type: 'srcset'
-                            }
-                        ],
-                    },
-                },
-            },
+        },
         ],
     },
     resolve: {
@@ -197,6 +197,7 @@ export default (env, options) => {
         webpackConfig.devServer = {
             watchFiles: ['src/**/*.html'],
             port: process.env.PORT,
+            host: '0.0.0.0',
             static: {
                 directory: path.join(__dirname, 'src/views'), // раздавать HTML в dev
                 publicPath: '/views',
