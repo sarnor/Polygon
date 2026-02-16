94bb44a7a8675b79bc062a9daac613079f830b1a
sarnor <sarnor@mail.ru>
2026-02-16 10:06:00 +0400

    16-02-2026 10:06

--- src/app/_head_index.js
+++ src/app/_head_index.js
@@ -3,3 +3,4 @@ import '../styles/less/index.less';
 import '../styles/scss/index.scss';
 import '../styles/sass/index.sass';
 import '../resource/fontawesome-pro-6.2.1-web/js/all.js';
+console.log('Head_log');
